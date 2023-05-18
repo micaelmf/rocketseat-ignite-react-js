3c4294219dc120bb92e6986a415ce905b2d6ec48
Micael Ferreira <micaelmf@gmail.com>
2023-05-17 22:52:11 -0300

    Create header and asidebar

---
+++ new_track/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,32 @@
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import './global.css'
+import styles from "./App.module.css"
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return (
+    <div>
+      
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Micael Ferreira"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aliquam voluptas, reprehenderit dolor, asperiores nihil nostrum vel qui dolorem quaerat molestias mollitia blanditiis velit id cupiditate voluptatem sint expedita exercitationem."
+          />
+          <Post 
+            author="Vanessa Pereira"
+            content="Outro post."
+          />
+          <Post />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
---
+++ new_track/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import { PencilLine } from 'phosphor-react';
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1614790871804-fe037bdc1214?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt="imagem de capa do usuário" 
+            />
+            <div className={styles.profile}>
+                <img src="https://github.com/micaelmf.png" alt="imagem de perfil do usuário" />
+                <strong>Micael Ferreira</strong>
+                <span>Web Developer</span>
+            </div>
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
