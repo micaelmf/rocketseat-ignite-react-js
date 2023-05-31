4f5e5f53d51415b85e2a635941ea5206ae92eee0
Micael Ferreira <micaelmf@gmail.com>
2023-05-30 21:21:57 -0300

    feat: finish module 2

--- new_track/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ new_track/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,24 +1,29 @@
 import { PencilLine } from 'phosphor-react';
-import styles from './Sidebar.module.css'
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
-    return (
-        <aside className={styles.sidebar}>
-            <img 
-                className={styles.cover}
-                src="https://images.unsplash.com/photo-1614790871804-fe037bdc1214?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt="imagem de capa do usuário" 
-            />
-            <div className={styles.profile}>
-                <img src="https://github.com/micaelmf.png" alt="imagem de perfil do usuário" />
-                <strong>Micael Ferreira</strong>
-                <span>Web Developer</span>
-            </div>
-            <footer>
-                <a href="#">
-                    <PencilLine size={20} />
-                    Editar seu perfil
-                </a>
-            </footer>
-        </aside>
-    );
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1614790871804-fe037bdc1214?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+        alt="imagem de capa do usuário"
+      />
+      <div className={styles.profile}>
+        <Avatar
+          src="https://github.com/micaelmf.png"
+          alt="imagem de perfil do usuário"
+        />
+        <strong>Micael Ferreira</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
