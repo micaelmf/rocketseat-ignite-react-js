8e637865093d50f46f7d0020420feed02a3c753d
Micael Ferreira <micaelmf@gmail.com>
2023-05-30 22:39:29 -0300

    feat: add keys

--- new_track/01-fundamentos-reactjs/src/App.jsx
+++ new_track/01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,49 @@
 import { Post } from './components/Post';
 import { Header } from './components/Header';
-import './global.css';
-import styles from './App.module.css';
 import { Sidebar } from './components/Sidebar';
+import styles from './App.module.css';
+import './global.css';
 
-function App() {
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/micaelmf.png',
+      name: 'Micael Ferreira',
+      role: 'Fullstack Develop',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'micaelferreira.com.br' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
+export function App() {
   return (
     <div>
       <Header />
@@ -12,12 +51,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Micael Ferreira"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aliquam voluptas, reprehenderit dolor, asperiores nihil nostrum vel qui dolorem quaerat molestias mollitia blanditiis velit id cupiditate voluptatem sint expedita exercitationem."
-          />
-          <Post author="Vanessa Pereira" content="Outro post." />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
